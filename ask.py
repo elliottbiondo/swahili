7ea32b002b2b0f1578dc50613fc42db12232b069
Elliott Biondo <biondoed@ornl.gov>
2021-02-06 14:56:25 -0500

    bidirectional

--- ask.py
+++ ask.py
@@ -2,7 +2,7 @@ from googletrans import Translator
 import pyttsx3
 from random import choice
 
-def trans(phrase):
+def translate(phrase):
     translator = Translator()
     return translator.translate(phrase, src="sw", dest="en").text
 
@@ -20,15 +20,34 @@ tense = ["na"]
 engine = pyttsx3.init()
 engine.setProperty('rate', 125)
 
-while (True):
+
+def print_result(expected, actual):
+    pass
+
+
+def swa_to_eng():
     phrase = choice(subjects) + choice(tense) + choice(verb_roots)
     engine.say(phrase)
     engine.runAndWait()
     val = input("Translate the following: {}\n>> ".format(phrase))
 
-    t = trans(phrase)
+    trans = translate(phrase)
 
-    if (val.lower() == t.lower()):
+    if (val.lower() == trans.lower()):
         print("CORRECT\n")
     else:
-        print("INCORRECT! answer: {}\n".format(t))
+        print("INCORRECT! answer: {}\n".format(trans))
+
+def eng_to_swa():
+    swa = choice(subjects) + choice(tense) + choice(verb_roots)
+    eng = translate(swa)
+    inp = input("Translate the following: {}\n>> ".format(eng))
+
+    if (inp.lower() == swa.lower()):
+        print("CORRECT\n")
+    else:
+        print("INCORRECT! answer: {}\n".format(swa))
+
+while (True):
+    eng_to_swa()
+    swa_to_eng()

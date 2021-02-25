07cc8771216a21bab2d2fb2403be6435802093f2
Elliott Biondo <biondoed@ornl.gov>
2021-02-24 21:38:39 -0500

    Input Parser file

--- ask.py
+++ ask.py
@@ -2,7 +2,7 @@ from googletrans import Translator
 import pyttsx3
 from random import choice
 
-from verb import Verb
+from parser import parse
 
 def translate(phrase):
     translator = Translator()
@@ -38,27 +38,7 @@ def eng_to_swa():
 
     check_result(swa, inp)
 
-verbs = []
-with open("vocab/verbs", 'r') as f:
-    lines = f.readlines()
-    i= 0
-    while i < len(lines):
-        if len(lines[i].strip()) > 0 and lines[i].strip()[0] != "#":
-            verb = Verb(lines[i].split("#")[0].strip().strip("-"))
-            i += 1
-            while lines[i].strip() != '':
-                line = lines[i].strip("#")
-                if line.strip()[0] == "exception":
-                    pos_neg, person, sing_plur, tense, value = line.strip()[1:]
-                    pos_neg = 0 if pos_neg == "pos" else 1
-                    person -= 1
-                    sing_plur = 0 if sing_plur == "sing" else 1
-                    verb.exceptions[(pos_neg, person, sing_plur, tense)] = value
-                i += 1
-            verbs.append(verb)
-        else:
-            i += 1
-
+verbs = parse()
 engine = pyttsx3.init()
 engine.setProperty('rate', 150)
 

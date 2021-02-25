946abe3a7dd1d4dd0c7f00ef65e9b92c96683289
Elliott Biondo <biondoed@ornl.gov>
2021-02-24 21:22:45 -0500

    Adding exceptions

--- ask.py
+++ ask.py
@@ -8,16 +8,6 @@ def translate(phrase):
     translator = Translator()
     return translator.translate(phrase, src="sw", dest="en").text
 
-verbs = []
-with open("vocab/verbs", 'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        if len(line.strip()) > 0 and line.strip()[0] != "#":
-            verbs.append(Verb(line.split("#")[0].strip().strip("-")))
-
-engine = pyttsx3.init()
-engine.setProperty('rate', 150)
-
 def check_result(expected, actual):
     if (expected.lower() == actual.lower()):
         print("CORRECT\n")
@@ -31,8 +21,8 @@ def swa_to_eng():
 
     eng = translate(swa)
 
-    #engine.say(swa)
-    #engine.runAndWait()
+    engine.say(swa)
+    engine.runAndWait()
 
     inp = input("Translate the following: {}\n>> ".format(swa))
 
@@ -47,6 +37,30 @@ def eng_to_swa():
     inp = input("Translate the following: {}\n>> ".format(eng))
 
     check_result(swa, inp)
+
+verbs = []
+with open("vocab/verbs", 'r') as f:
+    lines = f.readlines()
+    i= 0
+    while i < len(lines):
+        if len(lines[i].strip()) > 0 and lines[i].strip()[0] != "#":
+            verb = Verb(lines[i].split("#")[0].strip().strip("-"))
+            i += 1
+            while lines[i].strip() != '':
+                line = lines[i].strip("#")
+                if line.strip()[0] == "exception":
+                    pos_neg, person, sing_plur, tense, value = line.strip()[1:]
+                    pos_neg = 0 if pos_neg == "pos" else 1
+                    person -= 1
+                    sing_plur = 0 if sing_plur == "sing" else 1
+                    verb.exceptions[(pos_neg, person, sing_plur, tense)] = value
+                i += 1
+            verbs.append(verb)
+        else:
+            i += 1
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 150)
 
 while (True):
     a = choice([eng_to_swa(), swa_to_eng()])
--- verb.py
+++ verb.py
@@ -15,12 +15,18 @@ class Verb(object):
         self.subjects = [[["ni", "u", "a"], ["tu", "m", "wa"]],
                          [["si", "hu", "ha"], ["hatu", "ham", "hawa"]]]
 
-    def _conjugate(self, tense, person, plural, neg):
-        subject_pre = self.subjects[neg][plural][person]
-        tense_pre = self.tenses[neg][tense]
+        self.exceptions = {}
+
+    def _conjugate(self, pos_neg, person, sing_plur, tense):
+
+        if (pos_neg, person, sing_plur, tense) in self.exceptions.keys():
+            return self.exceptions[pos_neg, person, sing_plur, tense]
+
+        subject_pre = self.subjects[pos_neg][sing_plur][person]
+        tense_pre = self.tenses[pos_neg][tense]
         conj = subject_pre + tense_pre + self.root
 
-        if (tense == "present" and neg == 1 and self.root[-1] == "a"):
+        if (tense == "present" and pos_neg == 1 and self.root[-1] == "a"):
             return conj[:-1] + "i"
         else:
             return conj
@@ -28,11 +34,8 @@ class Verb(object):
     def conjugate(self):
         tense = choice(["past", "present", "future"])
         person = choice([0, 1, 2])
-        plural = choice([0, 1])
-        neg = choice([0, 1])
-        return self._conjugate(tense, person, plural, neg)
-
-
-
+        sing_plur = choice([0, 1])
+        pos_neg = choice([0, 1])
+        return self._conjugate(pos_neg, person, sing_plur, tense)
 
 
